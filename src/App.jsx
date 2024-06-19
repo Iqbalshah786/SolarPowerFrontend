211995841c96015ee3bc2fa27e6928c93c32d481
Iqbal Shah <iqbalshahandiri786@gmail.com>
2024-06-20 01:07:28 +0500

    add axios dependency and fetch sheet meta data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+/* eslint-disable no-unused-vars */
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("api/sheetmetadata/6672e92e2905b8f045e34543")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
       <h1 className=" text-red-700">hello</h1>
