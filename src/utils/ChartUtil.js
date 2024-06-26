525ea979aa5ef86857ea4b329250002c017e0f4e
Iqbal Shah <iqbalshahandiri786@gmail.com>
2024-06-26 17:21:47 +0500

    refactore code and implement different line for different levels i.e low,medium ,and high.

--- src/utils/ChartUtil.js
+++ src/utils/ChartUtil.js
@@ -1,34 +1,32 @@
 import { colors } from "../constants/index.js";
 
-const getChartData = (sheetData, selectedCountries, startYear, endYear) => {
+const getChartData = (sheetData, selectedCountry) => {
   const labels = sheetData.years.split(",");
 
-  const startIndex = labels.indexOf(startYear);
-  const endIndex = labels.indexOf(endYear);
-
-  const filteredLabels = labels.slice(startIndex, endIndex + 1);
-
-  const numDatasets = sheetData.datasets.length;
-  const numColors = colors.length;
-
-  if (numDatasets > numColors) {
-    alert.warn(
-      `Warning: Not enough colors (${numColors}) for datasets (${numDatasets}). Some colors will be reused.`
+  const datasets = ["Low", "Medium", "High"].map((level, index) => {
+    const dataset = sheetData.datasets.find(
+      (dataset) =>
+        dataset.country === selectedCountry && dataset.label === level
     );
-  }
 
-  const datasets = sheetData.datasets
-    .filter((dataset) =>
-      selectedCountries.some((country) => country.value === dataset.label)
-    )
-    .map((dataset, index) => ({
-      label: dataset.label,
-      data: dataset.data.slice(startIndex, endIndex + 1),
-      fill: false,
-      borderColor: colors[index % numColors],
-    }));
+    return dataset
+      ? {
+          label: level,
+          data: dataset.data.map((value) =>
+            parseInt(value.replace(/,/g, ""), 10)
+          ),
+          fill: false,
+          borderColor: colors[index],
+        }
+      : {
+          label: level,
+          data: [],
+          fill: false,
+          borderColor: colors[index],
+        };
+  });
 
-  return { labels: filteredLabels, datasets };
+  return { labels, datasets };
 };
 
 export { getChartData };
